e8dba7865b233c8a54d8a835ea4383da02d50481
ACHMAD DIDHANI <45660076+achmaddidhani@users.noreply.github.com>
2018-12-13 19:28:45 +0700

    Add files via upload

---
+++ LUAS PERSEGI PANJANG.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int p,l,luas;
+	printf("PROGRAM MENGHITUNG LUAS PERSEGI PANJANG\n");
+	printf("BY ACHMAD DIDHANI NIM 311810294\n");
+	printf("UNIVERSITAS PELITA BANGSA. TEKHNIK INFORMATIKA\n\n");
+	cout<<"masukan panjang:";
+	cin>>p;
+	cout<<"masukan lebar:";
+	cin>>l;
+	luas=p*l;
+	cout<<"luas persegi panjang adalah"<<luas;
+	return 0 ;
+	
+}
+	
+		
